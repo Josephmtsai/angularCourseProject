fbc25e030d8a437883d51b53b7fd09904af7bf1f
Unknown <mmx362003@gmail.com>
2018-12-06 00:02:53 +0800

    update for course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { contentsComponent } from './contents/contents.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, contentsComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/contents/contents.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'contents-component',
+  templateUrl: './contents.component.html'
+})
+export class contentsComponent {}
