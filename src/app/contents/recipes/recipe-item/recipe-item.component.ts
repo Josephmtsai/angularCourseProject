044dea9b7d54e0bec0683eab7a10a6119b24581e
Unknown <mmx362003@gmail.com>
2018-12-12 00:40:34 +0800

    temp fix issue

--- src/app/contents/recipes/recipe-item/recipe-item.component.ts
+++ src/app/contents/recipes/recipe-item/recipe-item.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import Recipe from '../recipe-common/recipe.model';
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.scss']
 })
 export class RecipeItemComponent implements OnInit {
+  recipes: Recipe[] = [new Recipe('Header', 'Description', 'http://www.sinaimg.cn/dy/slidenews/21_img/2014_47/17327_3856441_945009.jpg')];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/contents/recipes/recipe-list/recipe-list.component.ts
+++ src/app/contents/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from '../recipe-common/recipe.model';
+import Recipe from '../recipe-common/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +7,7 @@ import { Recipe } from '../recipe-common/recipe.model';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [new Recipe('Header', 'Description', 'http://www.sinaimg.cn/dy/slidenews/21_img/2014_47/17327_3856441_945009.jpg')];
   constructor() {}
 
   ngOnInit() {}
