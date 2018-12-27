8c906303dc19339b872f0599a68e68d8fea4adb0
Unknown <mmx362003@gmail.com>
2018-12-28 00:12:58 +0800

    add for component life cycle

--- src/app/contents/recipes/recipe-list/recipe-list.component.ts
+++ src/app/contents/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+} from '@angular/core';
 import Recipe from '../recipe-common/recipe.model';
 
 @Component({
@@ -6,14 +20,41 @@ import Recipe from '../recipe-common/recipe.model';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
   newRecipe: Recipe = new Recipe('', '', '');
   @Input('recipes') recipes: Recipe[];
   @Output() addRecipe = new EventEmitter<Recipe>();
   @Output() changeDisplayRecipe = new EventEmitter<Recipe>();
-  constructor() {}
+  constructor() {
+    console.log('constructor');
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges');
+    console.log(changes);
+  }
+  ngDoCheck() {
+    console.log('ngDoCheck');
+  }
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit');
+  }
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked');
+  }
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+  }
   displayDetail(recipe) {
     this.newRecipe = recipe;
   }
