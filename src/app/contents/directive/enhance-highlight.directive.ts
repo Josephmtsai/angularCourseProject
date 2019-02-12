28ddc572e081fcd1c6291f2ac0df868f1936c7ad
Joseph <mmx362003@gmail.com>
2019-02-12 16:41:43 +0800

    change to input value

--- src/app/contents/directive/enhance-highlight.directive.ts
+++ src/app/contents/directive/enhance-highlight.directive.ts
@@ -1,17 +1,19 @@
-import { Directive, OnInit, Renderer2, ElementRef, HostListener, HostBinding } from '@angular/core';
+import { Directive, OnInit, Renderer2, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 import { MockNgModuleResolver } from '@angular/compiler/testing';
 
 @Directive({
   selector: '[appEnHanceHighlight]'
 })
 export class EnHanceHighlightDirective implements OnInit {
-  @HostBinding('style.backgroundColor') background: string = 'transparent';
+  @Input() defaultColor: string = 'transparent';
+  @Input('appEnHanceHighlight') inputColor: string = 'blue';
+  @HostBinding('style.backgroundColor') background: string = this.defaultColor;
   constructor(private renderer: Renderer2, private elRef: ElementRef) {}
   ngOnInit() {}
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.background = 'Lime';
+    this.background = this.inputColor;
   }
   @HostListener('mouseleave') reset(eventData: Event) {
-    this.background = 'transparent';
+    this.background = this.defaultColor;
   }
 }
